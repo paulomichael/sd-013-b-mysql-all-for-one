741f32196678d7980934f1e372e1f9934f9dbbb3
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-10-19 14:35:44 -0300

    requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,1 @@
-CREATE TABLE IF NOT EXISTS `northwind`.`trybe` (
-  `A` INT(11) NULL DEFAULT NULL,
-  `Trybe` VARCHAR(50) NULL DEFAULT NULL,
-  `eh` INT(11) NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-INSERT INTO `trybe` (`A`, `Trybe`, `eh`) VALUES (5+6, 'de', 2+8);
-
-select * from northwind.trybe;
+SELECT 5+6 AS 'A', 'de' AS 'Trybe', 8+2 AS 'eh';
